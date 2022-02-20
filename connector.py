53ec5e7dcb8c78142f003ba3736560c64891f968
Deepayan Patra <dpatra@andrew.cmu.edu>
2022-02-19 22:43:35 -0500

    Add no-result execution + stats to DB connector

--- connector.py
+++ connector.py
@@ -12,17 +12,29 @@ class Connector():
         self._connection.autocommit = constants.AUTOCOMMIT
         logging.debug("Connected to {} as {}",
                       constants.DB_NAME, constants.DB_USER)
-        _ = self.exec_with_commit("create extension hypopg")
+        _ = self.exec_commit_no_result("CREATE EXTENSION hypopg")
         logging.debug("Enabled HypoPG")
+        self.refresh_stats()
 
     def set_autocommit(self, autocommit: bool):
         self._connection.autocommit = autocommit
 
-    def exec_with_commit(self, statement: str) -> list[str]:
+    def exec_commit_no_result(self, statement: str):
+        self._connection.execute(statement)
+        self._connection.commit()
+
+    def exec_commit(self, statement: str) -> list[str]:
         cur = self._connection.execute(statement)
         results = cur.fetchall()
         self._connection.commit()
         return results
+
+    def exec_transaction_no_result(self, statements: list[str]):
+        with self._connection.transaction():
+            cur = self._connection.cursor()
+            for stmt in statements:
+                cur.execute(stmt)
+        return res
 
     def exec_transaction(self, statements: list[str]) -> list[list[str]]:
         res = []
@@ -41,27 +53,30 @@ class Connector():
     # TODO: re-type this function once an index class exists
     def simulate_index(self, index: str) -> int:
         hypopg_stmt = f"SELECT * FROM hypopg_create_index(CREATE INDEX ON {index})"
-        result = self.exec_with_commit(hypopg_stmt)
+        result = self.exec_commit(hypopg_stmt)
         return result[0]
 
     def drop_simulated_index(self, oid: int):
         hypopg_stmt = f"SELECT * FROM hypopg_drop_index({oid})"
-        result = self.exec_with_commit(hypopg_stmt)
+        result = self.exec_commit(hypopg_stmt)
         assert(result[0] == True)
 
     def get_cost(self, query: str) -> float:
         stmt = f"EXPLAIN (format json) {query}"
-        plan = self.exec_with_commit(stmt)[0][0][0]["Plan"]
+        plan = self.exec_commit(stmt)[0][0][0]["Plan"]
         cost = plan["Total Cost"]
         return cost
 
-    # def get_db_info(self):
+    def refresh_stats(self):
+        self.exec_commit_no_result("ANALYZE")
+
+    # TODO: def get_db_info
 
 
 if __name__ == "__main__":
     db = Connector()
     q = "SELECT * FROM review r, item i WHERE i.i_id = r.i_id and r.i_id=112 ORDER BY rating DESC, r.creation_date DESC LIMIT 10"
-    res = db.exec_with_commit(q)
+    res = db.exec_commit(q)
     print(res)
     c = db.get_cost(q)
     print(c)
